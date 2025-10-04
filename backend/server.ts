cf3db807a8707d6c1d48118b65a56c1a0e0a5f45
Arpit-Verma5906 <vermaarpit.5906@gmail.com>
2025-10-04 23:42:22 +0530

    Implement Prisma for URL shortening functionality and update server logic

--- backend/server.ts
+++ backend/server.ts
@@ -1,54 +1,66 @@
 import express from "express";
 import type { Request, Response } from "express";
-import { connectDB, urls } from "./db"; // import connection
 import { nanoid } from "nanoid"
 import cors from "cors";
+import { PrismaClient, type Link } from "@prisma/client";
 
 const app = express();
-const port: number = 3000
-;
+const port: number = 3000;
+
+const prisma = new PrismaClient();
 
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend URL
+    origin: "http://localhost:5173", // your frontend URL
 }));
 // Middleware to parse JSON
 app.use(express.json());
 
-// connect to MongoDB
-await connectDB();
-
 // POST /shorten -> create a short URL
 app.post("/shorten", async (req: Request, res: Response) => {
-    const { longUrl } = req.body as { longUrl: string};
+    const { longUrl } = req.body as { longUrl: string };
 
-    if(!longUrl) {
-        return res.status(400).json({error: "longUrl is required" });
+    if (!longUrl) {
+        return res.status(400).json({ error: "longUrl is required" });
     }
 
     // Generate Unique ShortID
     let shortID = nanoid(6);
 
     // Check For Collisions
-    while (await urls.findOne({ shortID })) {
+    while (await prisma.link.findUnique({ where: { shortCode: shortID } })) {
         shortID = nanoid(6);
     }
 
     // Save Mapping
-    await urls.insertOne({ shortID, longUrl, createdAt: new Date(), expiryDate: new Date(Date.now() + 1000 * 60 * 60) });
+    const newLink: Link = await prisma.link.create({
+        data: {
+            url: longUrl,
+            shortCode: shortID,
+            createdAt: new Date(),
+            expiryDate: new Date(Date.now() +  60 * 1000),
+        },
+    })
 
-    return res.json({ shortUrl: `http://localhost:${port}/${shortID}`});
+    return res.json({ shortUrl: `http://localhost:${port}/${shortID}` });
 });
 
 // GET /:shortID -> redirect
 app.get("/:shortID", async (req: Request, res: Response) => {
-    const {shortID} = req.params;
-    const record = await urls.findOne({ shortID });
+    const { shortID } = req.params;
+    const record = await prisma.link.findUnique({
+        where: { shortCode: shortID },
+    });
 
-    if(!record) {
-        return res.status(404).json({ error: "Short URL not found"});
+    if (!record) {
+        return res.status(404).json({ error: "Short URL not found" });
     }
 
-    res.redirect(record.longUrl);
+    // Check expiry
+    if (record.expiryDate < new Date()) {
+        return res.status(410).json({ error: "Short URL expired" }); // 410 = Gone
+    }
+
+    res.redirect(record.url);
 });
 
 // GET route
